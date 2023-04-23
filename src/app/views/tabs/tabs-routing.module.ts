2215e88965fe96632746ecd2d5767f1f0f76d441
wermar <mail@marcelwernisch.de>
2023-04-24 00:08:16 +0200

    fix tabs issue and home screen

--- src/app/views/tabs/tabs-routing.module.ts
+++ src/app/views/tabs/tabs-routing.module.ts
@@ -1,24 +1,31 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {TabsPage} from "./tabs.page";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'training',
-    loadChildren: () => import('./training/training.module').then(m => m.TrainingPageModule)
-  },
-  {
-    path: 'check-in',
-    loadChildren: () => import('./check-in/check-in.module').then(m => m.CheckInPageModule)
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'training',
+        loadChildren: () => import('./training/training.module').then(m => m.TrainingPageModule)
+      },
+      {
+        path: 'check-in',
+        loadChildren: () => import('./check-in/check-in.module').then(m => m.CheckInPageModule)
+      },
+    ]
   },
   {
     path: '',
+    redirectTo: 'tabs/home',
     pathMatch: 'full',
-    redirectTo: 'home'
-  }
+  },
 ];
 
 @NgModule({
